d46915acc7dfca877fb34c3f28b592985642b377
SirErox <budadivino@gmail.com>
2024-03-20 15:49:52 -0600

    Se añadieron mas funciones
    Se añadieron librerias y se realizo la comprobacion de paso de argumentos, el comando para compilar es "gcc lexico.c -o lexico"
    
    y para ejecutar "lexico test.c /1

--- lexico.c
+++ lexico.c
@@ -1,8 +1,37 @@
 /** 
  * Etapa 1: lexico
  * Descripcion del programa: 
- * Autores:
+ * Autores: David Lozano Bañuelos
+ *          Bernardo Nava Trinidad
+ *          Andres...
 */
-int main(){
+//declaracion de librerias a usar
+#include <stdio.h>
+#include <stdlib.h>
+#include <stdbool.h>
+#include <ctype.h>
+#include <string.h>
+
+int main(int argc,char *argv[]){
+    FILE * archivo;
+    char caracter;
+    int num;
+    
+    // se obtiene el nombre del programa a comprobar
+    archivo=fopen(argv[1],"r");
+    if(archivo!=NULL){
+        printf("Abriendo archivo\n");
+    }
+    else{
+        printf("Error(1): Archivo no encontrado");
+        return 1;
+    }
+
+    if(sscanf(argv[2],"/%d",&num)==1)
+    //checamos si el argumento pasado es un numero valido o no
+        printf("numero pasado:%d\n",num);
+    else
+    //de no estar bien ingresado el numero dara error
+        printf("Formato de argumento invalido. \nDebe ser '/<numero>'");
     return 0;
 }
