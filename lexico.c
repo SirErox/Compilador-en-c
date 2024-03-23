9858a7276ead503bc0a09fba5fe498b9a295be1e
SirErox <budadivino@gmail.com>
2024-03-23 10:16:28 -0600

    Manejo de errores e improvisación del código
    Se improviso el código y se añadieron errores, tambien se añadieron validaciones para los argumentos y se coloco el switch para las funciones del programa...

--- lexico.c
+++ lexico.c
@@ -16,22 +16,46 @@ int main(int argc,char *argv[]){
     FILE * archivo;
     char caracter;
     int num;
-    
-    // se obtiene el nombre del programa a comprobar
+    //Validamos la cantidad de argumentos pasados al compilar, si no dara error
+    if(argc!=3){
+        printf("Error(0):Argumentos insuficientes\n\t Se requieren 2 argumentos");
+        return 1;
+    }else{
+    // se obtiene el nombre del programa a comprobar, el argumento r es para abrir el archivo en modo lectura pero solo si el archivo existe
     archivo=fopen(argv[1],"r");
     if(archivo!=NULL){
-        printf("Abriendo archivo\n");
+        //Validamos el numero pasado como argumento
+        if(sscanf(argv[2],"/%d",&num)==1 && argv[2]!=NULL){
+            //Aqui se continua con la ejecucion del programa, ya que se comprobaron los argumentos y el numero es valido
+            switch (num){
+            case 1:
+               printf("caso 1");
+                break;
+            case 2:
+            printf("caso 2");
+                break;
+            case 3:
+            printf("caso 3");
+                break;
+            case 4:
+            printf("caso 4");
+                break;
+            case 5:
+            printf("caso 5");
+                break;
+            default:
+                printf("Error(3):El numero '%d' debe estar entre el 1 y el 5.\n\t Favor de checar el numero ingresado.",num);
+                break;
+            }
+        } else{       
+            printf("Error(2):Formato de argumento invalido.\n\t Debe ser '/<numero>'");
+            return 1;
+        }
     }
     else{
-        printf("Error(1): Archivo no encontrado");
-        return 1;
+        printf("Error(1):Archivo '%s' no encontrado\n\t Favor de verificar el nombre del archivo, tambien se debe incluir la extension.",argv[1]);
+        return 1; //regresamos 1 cuando se encuentra un error al abrir el archivo
     }
-
-    if(sscanf(argv[2],"/%d",&num)==1)
-    //checamos si el argumento pasado es un numero valido o no
-        printf("numero pasado:%d\n",num);
-    else
-    //de no estar bien ingresado el numero dara error
-        printf("Formato de argumento invalido. \nDebe ser '/<numero>'");
+    }
     return 0;
 }
